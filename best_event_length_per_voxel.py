c448571da330e0c1f7f56a3e2dbf979acfd99ba8
jamalw <jamalw@princeton.edu>
2018-03-25 21:27:58 -0400

    deleted max_data variable

--- best_event_length_per_voxel.py
+++ best_event_length_per_voxel.py
@@ -2,32 +2,31 @@ import numpy as np
 import nibabel as nib
 from scipy import stats
 
-nii_template = nib.load('/tigress/jamalw/MES/subjects/MES_022817_0/analysis/run1.feat/trans_filtered_func_data.nii')
-datadir = '/tigress/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/'
+nii_template = nib.load('/jukebox/norman/jamalw/MES/subjects/MES_022817_0/analysis/run1.feat/trans_filtered_func_data.nii')
+datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/'
 
 # load,zscore,then store each dataset for each K in a list
-k_data = np.zeros((91,109,91,10))
-z_data = np.zeros((91,109,91,10))
+k_data = np.zeros((91,109,91,7))
+z_data = np.zeros((91,109,91,7))
 thresh = np.zeros((91,109,91))
-maxval_per_maxK = np.zeros((91,109,91,10)) 
+maxval_per_maxK = np.zeros((91,109,91,7)) 
+lengths = [10,15,20,25,30,35,40]
 
-for i in range(3,13):
-    data = nib.load(datadir + 'avg_real_k' + str(i) + '_across_songs.nii.gz').get_data()
-    z = nib.load(datadir + 'avg_z_k' + str(i) + '_across_songs.nii.gz').get_data()
-    k_data[:,:,:,i-3] = data
-    z_data[:,:,:,i-3] = z 
+for i in range(len(lengths)):
+    data = nib.load(datadir + str(lengths[i]) + '_sec_events.nii.gz').get_data()
+    z = nib.load(datadir + str(lengths[i]) + '_sec_events_Z.nii.gz').get_data()
+    k_data[:,:,:,i] = data
+    z_data[:,:,:,i] = z 
 
-max_data = np.max(k_data,axis=3)
-max_K = np.argmax(k_data,axis=3) + 3
+max_K = np.argmax(k_data,axis=3)
 max_K[np.sum(k_data, axis=3) == 0] = 0       
  
 for i in range(91):
     for j in range(109):
         for k in range(91):
-            thresh[i,j,k] = z_data[i,j,k,max_K[i,j,k]-3]
+            thresh[i,j,k] = z_data[i,j,k,max_K[i,j,k]]
 
-max_K[thresh < .1] = 0
-z_thresh = 0.02
+max_K[thresh < 0] = 0
 
 # save final map as nifti
 maxval = np.max(max_K)
@@ -35,6 +34,6 @@ minval = np.min(max_K)
 img = nib.Nifti1Image(max_K,affine = nii_template.affine)
 img.header['cal_min'] = minval
 img.header['cal_max'] = maxval
-nib.save(img, datadir + 'best_k_map.nii.gz')
+nib.save(img, datadir + 'best_event_length_map.nii.gz')
 
 
