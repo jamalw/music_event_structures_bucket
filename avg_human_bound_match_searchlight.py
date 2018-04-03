0f3a43524e970228e316e5071ebc1ecb57ea31c6
jamalw <jamalw@princeton.edu>
2018-04-03 13:52:49 -0400

    added average all maps together code and also changed indexing for single song holder variables

--- avg_human_bound_match_searchlight.py
+++ avg_human_bound_match_searchlight.py
@@ -8,16 +8,21 @@ songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Gu
 
 nii_template = nib.load('/jukebox/norman/jamalw/MES/subjects/MES_022817_0/analysis/run1.feat/trans_filtered_func_data.nii')
 
+avg_all_raw_songs = np.zeros((91,109,91))
+avg_all_z_songs = np.zeros((91,109,91))
+
 for i in range(len(songs)):
     fn_raw = glob.glob(datadir + songs[i] + '/raw/*.npy')
     fn_z   = glob.glob(datadir + songs[i] + '/zscores/*.npy')
     avg_raw = np.zeros((91,109,91))
-    avg_z   = np.zeros((91,109,91))
+    avg_z   = np.zeros((91,109,91)) 
     for j in range(len(fn_raw)):
         subj_raw = np.load(fn_raw[j])
         subj_z   = np.load(fn_z[j])
-        avg_raw += subj_raw/len(fn_raw)
-        avg_z   += subj_z/len(fn_z)
+        avg_raw[:,:,:] += subj_raw/len(fn_raw)
+        avg_z[:,:,:]   += subj_z/len(fn_z)
+        avg_all_raw_songs[:,:,:] += avg_raw/400
+        avg_all_z_songs[:,:,:]   += avg_z/400        
     # Save raw averages
     maxval = np.max(avg_raw)
     minval = np.min(avg_raw)
@@ -32,4 +37,21 @@ for i in range(len(songs)):
     img.header['cal_min'] = minval
     img.header['cal_max'] = maxval
     nib.save(img,datadir + songs[i] + '/avg_data/globals_avg_z_n25.nii.gz')  
-     
+    
+# Save all raw averages
+maxval = np.max(avg_all_raw_songs)
+minval = np.min(avg_all_raw_songs)
+img = nib.Nifti1Image(avg_all_raw_songs, affine=nii_template.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + 'globals_avg_all_raw_n25.nii.gz')  
+# Save all zscore averages
+maxval = np.max(avg_all_z_songs)
+minval = np.min(avg_all_z_songs)
+img = nib.Nifti1Image(avg_all_z_songs, affine=nii_template.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + '/globals_avg_all_z_n25.nii.gz')  
+    
+
+ 
