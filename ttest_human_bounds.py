e94b41fa3a9ec17caa0cc0d7dc13f9970deee7e1
jamalw <jamalw@princeton.edu>
2018-04-09 17:59:22 -0400

    added q value computation

--- ttest_human_bounds.py
+++ ttest_human_bounds.py
@@ -10,11 +10,47 @@ mask = nib.load('/jukebox/norman/jamalw/MES/prototype/link/scripts/mask_nonan.ni
 mask_reshaped = np.reshape(mask,(91*109*91)) != 0
 tmap_final1D = np.zeros((len(mask_reshaped)))
 pmap_final1D = np.zeros((len(mask_reshaped)))
+qmap_final1D = np.zeros((len(mask_reshaped)))
 
 songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
 
 all_songs3D = np.zeros((91,109,91,len(songs)))
 all_songs1D = np.zeros((218477,len(songs)))
+
+def FDR_p(pvals):
+    # Port of AFNI mri_fdrize.c
+    assert np.all(pvals>=0) and np.all(pvals<=1)
+    pvals[pvals < np.finfo(np.float_).eps] = np.finfo(np.float_).eps
+    pvals[pvals == 1] = 1-np.finfo(np.float_).eps
+    n = pvals.shape[0]
+
+    qvals = np.zeros((n))
+    sorted_ind = np.argsort(pvals)
+    sorted_pvals = pvals[sorted_ind]
+    qmin = 1.0
+    for i in range(n-1,-1,-1):
+        qval = (n * sorted_pvals[i])/(i+1)
+        if qval > qmin:
+            qval = qmin
+        else:
+            qmin = qval
+        qvals[sorted_ind[i]] = qval
+
+    # Estimate number of true positives m1 and adjust q
+    if n >= 233:
+        phist = np.histogram(pvals, bins=20, range=(0, 1))[0]
+        sorted_phist = np.sort(phist[3:19])
+        if np.sum(sorted_phist) >= 160:
+            median4 = n - 20*np.dot(np.array([1, 2, 2, 1]), sorted_phist[6:10])/6
+            median6 = n - 20*np.dot(np.array([1, 2, 2, 2, 2, 1]), sorted_phist[5:11])/10
+            m1 = min(median4, median6)
+
+            qfac = (n - m1)/n
+            if qfac < 0.5:
+                qfac = 0.25 + qfac**2
+            qvals *= qfac
+
+    return qvals
 
 for i in range(len(songs)):
     data = nib.load(datadir + songs[i] + '/avg_data/globals_avg_raw_n25.nii.gz').get_data()
@@ -23,15 +59,22 @@ for i in range(len(songs)):
 
 tmap1D = np.zeros((len(all_songs1D[:,0])))
 pmap1D = np.zeros((len(all_songs1D[:,0])))
+qmap1D = np.zeros((len(all_songs1D[:,0])))
 
 for j in range(len(all_songs1D[:,0])):
-    tmap1D[j],pmap1D[j] = stats.ttest_1samp(all_songs1D[j,:],0,axis=0)[0]
+    tmap1D[j],pmap1D[j] = stats.ttest_1samp(all_songs1D[j,:],0,axis=0)
 
+qmap1D = FDR_p(pmap1D)
+
+# Fit data back into whole brain
 tmap_final1D[mask_reshaped==1] = tmap1D
 tmap_final3D = np.reshape(tmap_final1D,(91,109,91))
 
 pmap_final1D[mask_reshaped==1] = pmap1D
 pmap_final3D = np.reshape(pmap_final1D,(91,109,91))
+
+qmap_final1D[mask_reshaped==1] = qmap1D
+qmap_final3D = np.reshape(qmap_final1D,(91,109,91))
 
 # save data
 maxval = np.max(tmap_final3D)
@@ -47,3 +90,10 @@ img = nib.Nifti1Image(pmap_final3D, affine=nii_template.affine)
 img.header['cal_min'] = minval
 img.header['cal_max'] = maxval
 nib.save(img,datadir + '/pstats_map.nii.gz')
+
+maxval = np.max(qmap_final3D)
+minval = np.min(qmap_final3D)
+img = nib.Nifti1Image(qmap_final3D, affine=nii_template.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + '/qstats_map.nii.gz')
