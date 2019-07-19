d7dea8bd69cc6e6384abec558689f339080c16b5
jamalw <jamalw@princeton.edu>
2019-07-19 16:56:25 -0400

    paired ratings and song names and sorted

--- beh_organizer.py
+++ beh_organizer.py
@@ -5,13 +5,15 @@ import pandas
 
 subjdir = '/jukebox/norman/jamalw/MES/subjects/'
 datadir = '/data/session_info/'
-fsuffix = '_prescan_num1.log'
 subj_ids = np.sort(fnmatch.filter(os.listdir(subjdir), 'MES_*'))
-ratings_clean = np.empty(48)
+ratings_clean = np.empty((2,48))
 
-for r in range(2):
+songs_run1 = []
+songs_run2 = []
+
+for run in range(2):
     # load in logfile
-    fsuffix = '_prescan_num' + str(r + 1) + '.log' 
+    fsuffix = '_prescan_num' + str(run + 1) + '.log' 
     
     with open(subjdir + subj_ids[0] + datadir + subj_ids[0] + fsuffix, newline='') as f:
         csvread = csv.reader(f)
@@ -24,15 +26,25 @@ for r in range(2):
 
     ratings = list(filter(None, ratings))
 
-    for i in range(len(ratings_clean)):
-        ratings_clean[i] = ratings[i][-1]
-
+    for i in range(ratings_clean.shape[1]):
+        ratings_clean[run,i] = ratings[i][-1]
+    
     # next get song names
-    songs_clean = []
-
     songs = [x for x in data if('.wav' in x)]
-
+   
     for i in range(16):
-        songs_clean.append(songs[i].rsplit('/',1)[1])
+        if run == 0:
+            songs_run1.append(songs[i].rsplit('/',1)[1])
+        elif run == 1:
+            songs_run2.append(songs[i].rsplit('/',1)[1])
+
+# split ratings into 16 (song) chunks of 3 (questions). this makes it easier to pair song names with ratings
+ratings1 = np.array_split(np.array(ratings_clean[0,:]),16)
+ratings2 = np.array_split(np.array(ratings_clean[1,:]),16)
+
+# format songs plus ratings(spr) as dictionary
+spr1 = dict(zip(songs_run1,ratings1))
+spr2 = dict(zip(songs_run2,ratings2))
+
 
 x = 10
