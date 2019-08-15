189e134e4f108001fae6bce616bca63695ab9b1e
jamalw <jamalw@princeton.edu>
2019-08-14 21:55:23 -0400

    currently changing searchlight function from hmm to rsa

--- music_feature_searchlight.py
+++ music_feature_searchlight.py
@@ -12,14 +12,14 @@ from sklearn import linear_model
 subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
 # run 1 times
-#song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511]) 
+song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511]) 
 
-#songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
+songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
 # run 2 times
-song_bounds = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
+#song_bounds = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
 
-songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
+#songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
 
 song_idx = int(sys.argv[1])
 srm_k = 30
@@ -30,9 +30,9 @@ mask_img = load_img(datadir + 'data/mask_nonan.nii.gz')
 mask = mask_img.get_data()
 mask_reshape = np.reshape(mask,(91*109*91))
 
-human_bounds = np.load(datadir + 'prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/' + songs[song_idx] + '/' + songs[song_idx] + '_beh_seg.npy') + hrf
+song_features = np.load(datadir + '/data/songs/music_features/' + songs[song_idx] + '/' + 'chroma.npy')
 
-def searchlight(coords,human_bounds,mask,subjs,song_idx,song_bounds,srm_k,hrf):
+def searchlight(coords,song_features,mask,subjs,song_idx,song_bounds,srm_k,hrf):
     
     """run searchlight 
 
@@ -87,7 +87,7 @@ def searchlight(coords,human_bounds,mask,subjs,song_idx,song_bounds,srm_k,hrf):
                print("Running Searchlight")
                # only run function on searchlights with voxels greater than or equal to min_vox
                if data[0].shape[0] >= min_vox: 
-                   SL_match = HMM(data,human_bounds,song_idx,song_bounds,srm_k,hrf)
+                   SL_match = RSA(data,song_features,song_idx,song_bounds,srm_k,hrf)
                    SL_results.append(SL_match)
                    SL_allvox.append(np.array(np.nonzero(SL_vox)[0])) 
     voxmean = np.zeros((coords.shape[0], nPerm+1))
@@ -101,11 +101,11 @@ def searchlight(coords,human_bounds,mask,subjs,song_idx,song_bounds,srm_k,hrf):
         vox_z[:,p] = (voxmean[:,p] - np.mean(voxmean[:,1:],axis=1))/np.std(voxmean[:,1:],axis=1) 
     return vox_z,voxmean
 
-def HMM(X,human_bounds,song_idx,song_bounds,srm_k,hrf):
+def RSA(X,song_features,song_idx,song_bounds,srm_k,hrf):
     
-    """fit hidden markov model
+    """compute RSA for music features and neural data
   
-       Fit HMM to average data and cross-validate with leftout subject using within song and between song average correlations              
+       compute similarity matrices for music and neural features then compute RSA between the two             
 
        Parameters
        ----------
@@ -117,20 +117,19 @@ def HMM(X,human_bounds,song_idx,song_bounds,srm_k,hrf):
  
        Returns
        -------
-       z: z-score after performing permuted cross-validation analysis      
+       z: z-score after permuting music feature vectors      
 
     """
     
-    w = 3
     nPerm = 1000
     run1 = [X[i] for i in np.arange(0, int(len(X)/2))]
     run2 = [X[i] for i in np.arange(int(len(X)/2), len(X))]
     print('Building Model')
     srm = SRM(n_iter=10, features=srm_k)   
     print('Training Model')
-    srm.fit(run1)
+    srm.fit(run2)
     print('Testing Model')
-    shared_data = srm.transform(run2)
+    shared_data = srm.transform(run1)
     shared_data = stats.zscore(np.dstack(shared_data),axis=1,ddof=1)
     data = np.mean(shared_data[:,song_bounds[song_idx]:song_bounds[song_idx + 1]],axis=2)
     nTR = data.shape[1]
@@ -167,7 +166,7 @@ z = np.reshape(z,(z.shape[0]*z.shape[1]*z.shape[2]))
 coords = np.vstack((x,y,z)).T 
 coords_mask = coords[mask_reshape>0]
 print('Running Distribute...')
-voxmean,real_sl_scores = searchlight(coords_mask,human_bounds,mask,subjs,song_idx,song_bounds,srm_k,hrf) 
+voxmean,real_sl_scores = searchlight(coords_mask,song_features,mask,subjs,song_idx,song_bounds,srm_k,hrf) 
 results3d[mask>0] = voxmean[:,0]
 results3d_real[mask>0] = real_sl_scores[:,0]
 for j in range(voxmean.shape[1]):
