9ebf639a98c6a10b36ac0ddf028a1c284ce0ed2d
jamalw <jamalw@princeton.edu>
2019-08-28 17:32:15 -0400

    now averaging both srm versions together

--- srm_hmm_fit_singles.py
+++ srm_hmm_fit_singles.py
@@ -33,8 +33,8 @@ start = song_bounds[song_number] + hrf
 end = song_bounds[song_number + 1] + hrf
 
 # Load in data
-train = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_bilateral_mpfc_run1_n25.npy'),axis=1,ddof=1))
-test = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_bilateral_mpfc_run2_n25.npy'),axis=1,ddof=1))
+train = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_left_AG_run1_n25.npy'),axis=1,ddof=1))
+test = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_left_AG_run2_n25.npy'),axis=1,ddof=1))
 
 # Convert data into lists where each element is voxels by samples
 train_list = []
@@ -47,53 +47,55 @@ n_iter = 50
 features = 10
 # Initialize model
 print('Building Model')
-srm = SRM(n_iter=n_iter, features=features)
+srm_train_run1 = SRM(n_iter=n_iter, features=features)
+srm_train_run2 = SRM(n_iter=n_iter, features=features)
 
 # Fit model to training data (run 1)
 print('Training Model')
-srm.fit(train_list)
+srm_train_run1.fit(train_list)
+srm_train_run2.fit(test_list)
 
 # Test model on testing data to produce shared response
 print('Testing Model')
-shared_data = srm.transform(test_list)
+shared_data_train_run1 = srm_train_run1.transform(test_list)
+shared_data_train_run2 = srm_train_run2.transform(train_list)
 
-avg_response = sum(shared_data)/len(shared_data)
+avg_response_train_run1 = sum(shared_data_train_run1)/len(shared_data_train_run1)
+avg_response_train_run2 = sum(shared_data_train_run2)/len(shared_data_train_run2)
 
-nR = shared_data[0].shape[0]
-nTR = shared_data[0][:,start:end].shape[1]
-nSubj = len(shared_data)
+avg_response = (avg_response_train_run1 + avg_response_train_run2)/2
 
-ev = brainiak.eventseg.event.EventSegment(len(human_bounds) - 1)
-ev.fit(avg_response[:,start:end].T)
+nTR = shared_data_train_run1[0][:,start:end].shape[1]
+nSubj = 25
 
-bounds = np.where(np.diff(np.argmax(ev.segments_[0], axis=1)))[0]
-
+# Fit HMM
+#ev = brainiak.eventseg.event.EventSegment(len(human_bounds) - 1)
+#ev.fit(avg_response[:,start:end].T)
+#bounds = np.where(np.diff(np.argmax(ev.segments_[0], axis=1)))[0]
+#bounds_aug = np.concatenate(([0],bounds,[nTR]))
+#
 plt.figure(figsize=(10,10))
 plt.imshow(np.corrcoef(avg_response[:,start:end].T))
 plt.colorbar()
 ax = plt.gca()
-bounds_aug = np.concatenate(([0],bounds,[nTR]))
-for i in range(len(bounds_aug)-1):
-    rect1 = patches.Rectangle((bounds_aug[i],bounds_aug[i]),bounds_aug[i+1]-bounds_aug[i],bounds_aug[i+1]-bounds_aug[i],linewidth=3,edgecolor='w',facecolor='none',label='Model Fit')
-    ax.add_patch(rect1)
 
+# Plot HMM boundaries
+#for i in range(len(bounds_aug)-1):
+#    rect1 = patches.Rectangle((bounds_aug[i],bounds_aug[i]),bounds_aug[i+1]-bounds_aug[i],bounds_aug[i+1]-bounds_aug[i],linewidth=3,edgecolor='w',facecolor='none',label='Model Fit')
+#    ax.add_patch(rect1)
+
+# Plot human boundaries
 for i in range(len(human_bounds)-1):
     rect2 = patches.Rectangle((human_bounds[i],human_bounds[i]),human_bounds[i+1]-human_bounds[i],human_bounds[i+1]-human_bounds[i],linewidth=3,edgecolor='k',facecolor='none',label='Human Annotations')
     ax.add_patch(rect2)
 
 song_titles = ['St Pauls Suite', 'I Love Music', 'Moonlight Sonata', 'Change of the Guard','Waltz of Flowers','The Bird', 'Island', 'Allegro Moderato', 'Finlandia', 'Early Summer', 'Capriccio Espagnole', 'Symphony Fantastique', 'Boogie Stop Shuffle', 'My Favorite Things', 'Blue Monk','All Blues']
 
-plt.title('HMM Fit to mPFC for ' + song_titles[song_number],fontsize=18,fontweight='bold')
+plt.title('HMM Fit to lPrecunues for ' + song_titles[song_number],fontsize=18,fontweight='bold')
 plt.xlabel('TRs',fontsize=18,fontweight='bold')
 plt.ylabel('TRs',fontsize=18,fontweight='bold')
 plt.legend(handles=[rect1,rect2])
 
-plt.savefig('plots/' + songs[song_number] + '_srm_k_' + str(features) + '_iter_' + str(n_iter))
+#plt.savefig('plots/' + songs[song_number] + '_srm_k_' + str(features) + '_iter_' + str(n_iter))
 
-#for i in range(len(human_bounds2)-1):
-#    plt.figure(i+1)
-#    plt.imshow(np.corrcoef(avg_response[:,human_bounds2[i]:human_bounds2[i+1]].T))
-#    plt.title(songs[i] + ' A1 SRM',fontsize=18)
-#    plt.xlabel('TRs',fontsize=18,fontweight='bold')
-#    plt.ylabel('TRs',fontsize=18,fontweight='bold')    
-#    plt.savefig('plots/'+songs[i] + '_A1_SRM') 
+
