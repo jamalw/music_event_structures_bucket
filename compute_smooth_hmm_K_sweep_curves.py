5a6e15bab65639ce2cd47ebe099c4e2b3d5f7ba0
jamalw <jamalw@princeton.edu>
2020-02-12 19:43:49 -0500

    finding best sigma and saving curves in single array

--- compute_smooth_hmm_K_sweep_curves.py
+++ compute_smooth_hmm_K_sweep_curves.py
@@ -24,7 +24,7 @@ prec_data = np.dstack(prec_data_list)
 a1_data = np.dstack(a1_data_list)
 AG_data = np.dstack(AG_data_list)
 
-sigma = '3'
+sigma = '5'
 
 durs_run1 = np.array([225,90,180,135,90,180,135,90,180,135,225,90,225,225,180,135])
 
@@ -37,30 +37,37 @@ event_lengths = durs_run1_new/fairK
 unique_event_lengths = np.unique(event_lengths)
 x = event_lengths.ravel()
 
+ROI_data = [a1_data, AG_data, prec_data]
+
+a1_gt_AG = 0
+a1_gt_prec = 0
+test_x = np.linspace(min(x), max(x), num=100)
+smooth_wva = np.zeros((len(unique_event_lengths), len(ROI_data), nBoots))
+
 for b in range(nBoots):
-    y_a1 = a1_data[:,:,b].ravel()
-    y_AG = AG_data[:,:,b].ravel()
-    y_prec = prec_data[:,:,b].ravel()
+    # Optimize bandwidth
+    opt_bw = 0
+    for ROI in range(len(ROI_data)):
+        y = ROI_data[ROI][:,:,b].ravel()
+        KR = KernelReg(y,x,var_type='c')
+        opt_bw += KR.bw/len(ROI_data)
 
-KR = KernelReg(y_a1,x,var_type='c', bw=sigma)
-smooth_y_a1 = KR.fit(unique_event_lengths)
-plt.plot(unique_event_lengths, smooth_y_a1[0], color='red', label='A1')
+    max_wva = np.zeros(len(ROI_data))
+    for ROI in range(len(ROI_data)):
+        y = ROI_data[ROI][:,:,b].ravel()
+        KR = KernelReg(y,x,var_type='c', bw=opt_bw)
+        max_wva[ROI] = np.argmax(KR.fit(test_x)[0])  # Find peak on fine grid
+        smooth_wva[:, ROI, b] += KR.fit(unique_event_lengths)[0]
 
-KR = KernelReg(y_AG,x,var_type='c', bw=sigma)
-smooth_y_AG = KR.fit(unique_event_lengths)
-plt.plot(unique_event_lengths, smooth_y_AG[0], color='magenta', label='AG')
-
-KR = KernelReg(y_prec,x,var_type='c', bw=sigma)
-smooth_y_prec = KR.fit(unique_event_lengths)
-plt.plot(unique_event_lengths, smooth_y_prec[0], label='prec')
+np.save(datadir + 'smooth_wva',smooth_wva)
 
 
-plt.legend()
+#plt.legend()
 
 #plt.xticks(x,unique_event_lengths,rotation=45)
-plt.xlabel('Event Length (s)', fontsize=18)
-plt.ylabel('WvA Score', fontsize=18)
-plt.title('ROIs Preferred Event Length', fontsize=18)
-plt.tight_layout()
+#plt.xlabel('Event Length (s)', fontsize=18)
+#plt.ylabel('WvA Score', fontsize=18)
+#plt.title('ROIs Preferred Event Length', fontsize=18)
+#plt.tight_layout()
 
 #plt.savefig('hmm_K_sweep_paper_results/preferred_event_length')
