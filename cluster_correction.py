f3e97be7687b74b0b1536ddc65de6bbffbf60b5d
jamalw <jamalw@princeton.edu>
2020-06-20 14:32:41 -0400

    added plotting function for debugging

--- cluster_correction.py
+++ cluster_correction.py
@@ -1,14 +1,30 @@
 from scipy.ndimage import label, generate_binary_structure
 import numpy as np
 import nibabel as nib
+import matplotlib.pyplot as plt
+from nilearn import surface, datasets, plotting
 
-datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_bound_match_shuffle_event_lengths_rotation/ttest_results/'
 
-fn = 'pstats_map_both_runs_w_perms.npy'
+def plot_data(data,affine,plot_title,thresh):
+    minval = np.min(data)
+    maxval = np.max(data)
+    img = nib.Nifti1Image(data,affine)
+    img.header["cal_min"] = minval
+    img.header["cal_max"] = maxval
+    fsaverage = datasets.fetch_surf_fsaverage()
+    texture_right = surface.vol_to_surf(img, fsaverage.pial_right) 
+    texture_left = surface.vol_to_surf(img, fsaverage.pial_left)
+    plotting.plot_surf_stat_map(fsaverage.infl_right, texture_right, hemi='right', title=plot_title, colorbar=True, threshold=thresh, bg_map=fsaverage.sulc_right)    
+    plotting.plot_surf_stat_map(fsaverage.infl_left, texture_left, hemi='left', title=plot_title, threshold=thresh, bg_map=fsaverage.sulc_left)
+    plt.show() 
 
-mask = nib.load('/jukebox/norman/jamalw/MES/data/mask_nonan.nii.gz')
+datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/'
+
+mask = nib.load('/jukebox/norman/jamalw/MES/data/mask_nonan.nii')
 
 affine = mask.affine
+
+fn = 'ttest_results_test/pstats_map_both_runs_w_perms.npy'
 
 print("Loading Data")
 pvals = np.load(datadir + fn)
@@ -56,4 +72,4 @@ maxval = np.max(image)
 img = nib.Nifti1Image(image, affine=affine)
 img.header['cal_min'] = minval
 img.header['cal_max'] = maxval
-nib.save(img,'/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_bound_match_shuffle_event_lengths_rotation/cluster_corrected_p05')
+nib.save(img,datadir + 'cluster_corrected_005.nii.gz')
