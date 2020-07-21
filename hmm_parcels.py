ab29b647c614aa4b8f430333a788df11f2d9cf35
jamalw <jamalw@princeton.edu>
2020-07-21 16:53:14 -0400

    now getting song end and start times

--- hmm_parcels.py
+++ hmm_parcels.py
@@ -8,8 +8,7 @@ from brainiak.funcalign.srm import SRM
 import os
 from sklearn import linear_model
 
-#idx = int(sys.argv[1])
-#song = songs[int(idx)]
+idx = int(sys.argv[1])
 
 def clean_data(data,motion):
     motion = motion.T
@@ -41,6 +40,56 @@ def srm(run1,run2):
 
     return run1, run2
     
+def HMM(X,human_bounds,song_idx,song_bounds,srm_k,hrf):
+
+    """fit hidden markov model
+  
+       Fit HMM to average data and cross-validate with leftout subject using within song and between song average correlations              
+
+       Parameters
+       ----------
+       A: voxel by time ndarray (2D)
+       B: voxel by time ndarray (2D)
+       C: voxel by time ndarray (2D)
+       D: voxel by time ndarray (2D)
+       K: # of events for HMM (scalar)
+ 
+       Returns
+       -------
+       z: z-score after performing permuted cross-validation analysis      
+
+    """
+
+    w = 3
+    nPerm = 1000
+    run1 = [X[i] for i in np.arange(0, int(len(X)/2))]
+    run2 = [X[i] for i in np.arange(int(len(X)/2), len(X))]
+    print('Building Model')
+    srm = SRM(n_iter=10, features=srm_k)
+    print('Training Model')
+    srm.fit(run2)
+    print('Testing Model')
+    shared_data = srm.transform(run1)
+    shared_data = stats.zscore(np.dstack(shared_data),axis=1,ddof=1)
+    data = np.mean(shared_data[:,song_bounds[song_idx]:song_bounds[song_idx + 1]],axis=2)
+    nTR = data.shape[1]
+
+    # Fit to all but one subject
+    K = len(human_bounds) + 1
+    ev = brainiak.eventseg.event.EventSegment(K)
+    ev.fit(data.T)
+    bounds = np.where(np.diff(np.argmax(ev.segments_[0],axis=1)))[0]
+    match = np.zeros(nPerm+1)
+    events = np.argmax(ev.segments_[0],axis=1)
+    _, event_lengths = np.unique(events, return_counts=True)
+    perm_bounds = bounds.copy()
+
+    for p in range(nPerm+1):
+        match[p] = sum([np.min(np.abs(perm_bounds - hb)) for hb in human_bounds])
+        np.random.seed(p)
+        perm_lengths = np.random.permutation(event_lengths)
+        events = np.zeros(nTR, dtype=np.int)
+        events[np.cumsum(perm_lengths[:-1])] = 1
 
 
 # run 1 times
@@ -48,10 +97,20 @@ song_bounds1 = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749
 
 songs1 = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
+
+# get song start time and end time for run 1
+start_idx_run1 = song_bounds1[idx]
+end_idx_run1   = song_bounds1[idx+1] 
+
 # run 2 times
 song_bounds2 = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
 
 songs2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
+
+# get song start time and end time for run 2
+song_name = songs1[idx]
+start_idx_run2 = song_bounds2[songs2.index(song_name)]
+end_idx_run2   = song_bounds2[songs2.index(song_name) + 1]  
 
 #subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
@@ -87,6 +146,5 @@ for s in range(len(subjs)):
     run2_masked.append(run2[indices][:])
 
 run1_SRM, run2_SRM = srm(run1_masked,run2_masked)
-
 
 x=10
