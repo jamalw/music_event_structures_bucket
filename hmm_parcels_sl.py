cd034a4dde43566cde335f70f4b902a6c681e846
jamalw <jamalw@princeton.edu>
2020-07-27 20:55:14 -0400

    removed data cleaning code and fixed issue where non-clean data was being used for hmm fitting

--- hmm_parcels_sl.py
+++ hmm_parcels_sl.py
@@ -10,14 +10,6 @@ from sklearn import linear_model
 
 idx = int(sys.argv[1])
 
-def clean_data(data,motion):
-    nTR = data.shape[1]
-    motion = motion.T
-    regr = linear_model.LinearRegression()
-    regr.fit(motion[:,0:nTR].T, data[:,:].T)
-    clean_data = data[:,:] - np.dot(regr.coef_, motion[:,0:nTR]) - regr.intercept_[:, np.newaxis]
-    return clean_data  
-
 def srm(run1,run2):
     # initialize model
     print('Building Models')
@@ -119,12 +111,7 @@ song_name = songs1[idx]
 start_idx_run2 = song_bounds2[songs2.index(song_name)]
 end_idx_run2   = song_bounds2[songs2.index(song_name) + 1]  
 
-#subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
-
-subjs = ['MES_022817_0','MES_030217_0', 'MES_032117_1','MES_040217_0','MES_041117_0']
-
 datadir = '/jukebox/norman/jamalw/MES/'
-motion_dir = datadir + '/prototype/link/scripts/data/searchlight_input/'
 
 mask_img = nib.load(datadir + 'data/mask_nonan.nii')
 
@@ -140,33 +127,16 @@ parcels = nib.load(datadir + "data/CBIG/stable_projects/brain_parcellation/Schae
 pvals = np.zeros_like(mask_img.get_data(),dtype=float)
 match = np.zeros_like(mask_img.get_data())
 
-for i in range(3):
-#for i in range(int(np.max(parcels))):
+parcel_dir = datadir + "prototype/link/scripts/data/searchlight_input/parcels/Schaefer200/"
+
+for i in range(int(np.max(parcels))):
+    print("Parcel Num: ", str(i+1))
     # get indices where mask and parcels overlap
     indices = np.where((mask_img.get_data() > 0) & (parcels == i + 1))
 
     # initialize list for storing masked data across subjects
-    run1_masked = []
-    run2_masked = []
-
-    for s in range(len(subjs)):
-        # Load subjects nifti and motion data then clean (run1)
-        print("Loading Run1 BOLD subj num: " + str(s+1))
-        run1 = nib.load(datadir + 'subjects/' + subjs[s] + '/analysis/run1.feat/trans_filtered_func_data.nii').get_data()[:,:,:,0:2511]
-        print("Loading Run1 Motion Regressors")
-        motion_run1 = np.genfromtxt(motion_dir + subjs[s] + '/EPI_mcf1.par')
-        print("Cleaning Run1 BOLD Data")
-        clean_run1 = stats.zscore(clean_data(run1[indices][:], motion_run1), axis=1, ddof=1)
-        run1_masked.append(run1[indices][:])
-   
-        # Load subjects nifti and motion data then clean (run2)
-        print("Loading Run2 BOLD subj num: " + str(s+1)) 
-        run2 = nib.load(datadir + 'subjects/' + subjs[s] + '/analysis/run2.feat/trans_filtered_func_data.nii').get_data()[:,:,:,0:2511]
-        print("Loading Run2 Motion Regressors")
-        motion_run2 = np.genfromtxt(motion_dir + subjs[s] + '/EPI_mcf2.par')
-        print("Cleaning Run2 BOLD Data")
-        clean_run2 = stats.zscore(clean_data(run2[indices][:], motion_run2), axis=1, ddof=1)
-        run2_masked.append(run2[indices][:])
+    run1_SRM = np.load(parcel_dir + "parcel" + str(i+1) + "_run1.npy")
+    run2_SRM = np.load(parcel_dir + "parcel" + str(i+1) + "_run2.npy")
     
     # run SRM on masked data
     run1_SRM, run2_SRM = srm(run1_masked,run2_masked)
