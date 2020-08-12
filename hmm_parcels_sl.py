1196e2e3ea217ea3655d6cd3ffd936350861e8a2
jamalw <jamalw@princeton.edu>
2020-08-12 14:42:44 -0400

    added srm version 1 code

--- hmm_parcels_sl.py
+++ hmm_parcels_sl.py
@@ -6,10 +6,12 @@ import brainiak.eventseg.event
 from scipy.stats import norm,zscore,pearsonr,stats
 import os
 from sklearn import linear_model
-from srm import SRM_V2
+from srm import SRM_V1, SRM_V2, SRM_V3
 import scipy.stats as st
+import matplotlib.pyplot as plt
 
 idx = int(sys.argv[1])
+runNum = int(sys.argv[2])
 
 def save_nifti(data,affine,savedir):
     minval = np.min(data)
@@ -55,6 +57,7 @@ def HMM(X,human_bounds):
 
     for p in range(nPerm+1):
         #match[p] = sum([np.min(np.abs(perm_bounds - hb)) for hb in human_bounds])
+        #match[p] = np.sqrt(sum([np.min((perm_bounds - hb)**2) for hb in human_bounds]))
         for hb in human_bounds:
             if np.any(np.abs(perm_bounds - hb) <= w):
                 match[p] += 1
@@ -75,68 +78,66 @@ def HMM(X,human_bounds):
 
     return match
 
+hrf = 4
 
-# run 1 times
-song_bounds1 = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511])
+if runNum == 0:
+    # run 1 times
+    song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511])
 
-songs1 = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
+    songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
 
-# get song start time and end time for run 1
-start_idx_run1 = song_bounds1[idx]
-end_idx_run1   = song_bounds1[idx+1] 
+    # get song start time and end time for run 1
+    start_idx = song_bounds[idx] 
+    end_idx   = song_bounds[idx+1]
 
-# run 2 times
-song_bounds2 = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
+elif runNum == 1:
+    # run 2 times
+    song_bounds = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
 
-songs2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
+    songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
 
-# get song start time and end time for run 2
-song_name = songs1[idx]
-start_idx_run2 = song_bounds2[songs2.index(song_name)]
-end_idx_run2   = song_bounds2[songs2.index(song_name) + 1]  
+    # get song start time and end time for run 2
+    start_idx = song_bounds[idx]
+    end_idx   = song_bounds[idx+1]
 
 datadir = '/jukebox/norman/jamalw/MES/'
 
 mask_img = nib.load(datadir + 'data/mask_nonan.nii')
 
-hrf = 5
 n_iter = 50
 srm_k = 30
 
 # load human boundaries
-human_bounds = np.load(datadir + 'prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/' + song_name + '/' + song_name + '_beh_seg.npy') + hrf
+human_bounds = np.load(datadir + 'prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/' + songs[idx] + '/' + songs[idx] + '_beh_seg.npy') + hrf
 
-#human_bounds = np.array([12,25,58,69,98,104,139,165,194,203,209,222])
-
-parcels = nib.load(datadir + "data/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_200Parcels_17Networks_order_FSLMNI152_2mm.nii.gz").get_data()
+parcels = nib.load(datadir + "data/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_100Parcels_17Networks_order_FSLMNI152_2mm.nii.gz").get_data()
 
 # create brain-like object to store data into
 pvals = np.zeros_like(mask_img.get_data(),dtype=float)
 match = np.zeros_like(mask_img.get_data())
 
-parcel_dir = datadir + "prototype/link/scripts/data/searchlight_input/parcels/Schaefer200/"
+parcel_dir = datadir + "prototype/link/scripts/data/searchlight_input/parcels/Schaefer100/"
+
+savedir = "/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/parcels/Schaefer100/" + songs[idx]
 
 for i in range(int(np.max(parcels))):
-#for i in [124]:
     print("Parcel Num: ", str(i+1))
     # get indices where mask and parcels overlap
     indices = np.where((mask_img.get_data() > 0) & (parcels == i + 1))
-
+ 
     # initialize list for storing masked data across subjects
     run1 = np.load(parcel_dir + "parcel" + str(i+1) + "_run1.npy")
     run2 = np.load(parcel_dir + "parcel" + str(i+1) + "_run2.npy")
     
     # run SRM on masked data
-    run1_SRM, run2_SRM = SRM_V2(run1,run2,srm_k,n_iter)
+    if runNum == 0:
+        shared_data = SRM_V1(run1,run2,srm_k,n_iter)
+    elif runNum == 1:
+        shared_data = SRM_V1(run2,run1,srm_k,n_iter)
 
-    # get song data from each run
-    data_run1 = run1_SRM[:,start_idx_run1:end_idx_run1]
-    data_run2 = run2_SRM[:,start_idx_run2:end_idx_run2]
-
-    # average song data between two runs
-    data = (data_run1 + data_run2) / 2
-
+    data = np.mean(stats.zscore(np.dstack(shared_data),axis=1,ddof=1),axis=2)[:,start_idx:end_idx]
+    
     # fit HMM to song data and return match data where first entry is true match score and all others are permutation scores
     print("Fitting HMM")
     SL_match = HMM(data,human_bounds)
@@ -154,10 +155,14 @@ for i in range(int(np.max(parcels))):
     pvals[indices] = match_p  
     match[indices] = match_z 
 
-savedir = "/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/parcels/Schaefer200/" + song_name
 
-pfn = savedir + "/pvals"
-mfn = savedir + "/match_scores"
+if runNum == 0:
+    pfn = savedir + "/pvals_srm_v1_test_run2"
+    mfn = savedir + "/match_scores_srm_v1_test_run2"
+elif runNum == 1:
+    pfn = savedir + "/pvals_srm_v1_test_run1"
+    mfn = savedir + "/match_scores_srm_v1_test_run1"
+
 
 save_nifti(pvals, mask_img.affine, pfn) 
 save_nifti(match, mask_img.affine, mfn)
