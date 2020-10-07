6855ef8dbfd1d2db2371865c9e4e6e4ed8fda26b
jamalw <jamalw@princeton.edu>
2020-10-07 16:51:18 -0400

    reshaping data and changed nboots back to 50

--- hmm_K_sweep_paper.py
+++ hmm_K_sweep_paper.py
@@ -14,7 +14,7 @@ import sys
 def fisher_mean(correlation, axis=None):
     return np.tanh(np.mean(np.arctanh(correlation), axis=axis))
 
-datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/chris_dartmouth/data/'
+datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_input/parcels/Schaefer300/'
 ann_dirs = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/'
 
 bootNum = int(sys.argv[1])
@@ -38,9 +38,12 @@ songs_run2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_t
 
 durs_run2 = np.array([90,180,180,90,135,180,180,225,225,135,90,135,225,225,90,135])
 
-# Load in data
-run1 = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_left_AG_run1_n25.npy'),axis=1,ddof=1))
-run2 = np.nan_to_num(stats.zscore(np.load(datadir + 'zstats_human_bounds_left_AG_run2_n25.npy'),axis=1,ddof=1))
+# Load in data and reshape for Schaefer parcellations where the dimensionality is nSubjs X nVox X Time whereas the dimensions for the data used in the original version of the analysis was nVox X Time X nSubjs 
+run1 = np.load(datadir + 'parcel115_run1.npy')
+run2 = np.load(datadir + 'parcel115_run2.npy')
+
+run1 = np.reshape(run1,(run1.shape[1],run1.shape[2],run1.shape[0]))
+run2 = np.reshape(run2,(run2.shape[1],run2.shape[2],run2.shape[0]))
 
 nSubj = run1.shape[2]
 
@@ -53,7 +56,7 @@ for i in range(0,nSubj):
 
 run1_list_orig = run1_list.copy()
 run2_list_orig = run2_list.copy()
-nboot = 1000
+nboot = 50
 
 wVa_results = np.zeros((16,len(K_set),nboot))
 
@@ -124,4 +127,4 @@ for b in range(nboot):
                     wVa_results[i,j,b] = within_across
 
 
-np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/hmm_K_sweep_paper_results/AG_wva' + str(bootNum), wVa_results)
+np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/hmm_K_sweep_paper_results/Schaefer300/lprec_wva' + str(bootNum), wVa_results)
