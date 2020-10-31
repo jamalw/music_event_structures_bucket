b62aeccec7e6e4c833bd7dd15d1b99a1c955ff8d
jamalw <jamalw@princeton.edu>
2020-10-31 16:20:59 -0400

    set primary variables at top now

--- sl_masker_paper.py
+++ sl_masker_paper.py
@@ -2,6 +2,9 @@ import numpy as np
 import nibabel as nib
 
 parcelNum = 100
+thr = '001'
+idx = 61
+roi_name = 'rA1'
 
 def save_nifti(data,affine,savedir):
     minval = np.min(data)
@@ -13,11 +16,11 @@ def save_nifti(data,affine,savedir):
     nib.save(img, savedir)
  
 datadir = '/jukebox/norman/jamalw/MES/'
-savedir = datadir + "/data/fdr_005_human_bounds_left_AG_bin.nii.gz"
+savedir = datadir + "/data/fdr_" + thr + "_human_bounds_" + roi_name + "_bin.nii.gz"
 
 full_mask_img = nib.load(datadir + 'data/mask_nonan.nii')
 
-stats_mask = nib.load(datadir + 'prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/masks/qstats_no_motion_005_mask.nii.gz')
+stats_mask = nib.load(datadir + 'prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/masks/qstats_no_motion_' + thr + '_mask.nii.gz')
 
 parcels = nib.load(datadir + "data/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_" + str(parcelNum) + "Parcels_17Networks_order_FSLMNI152_2mm.nii.gz").get_data()
 
@@ -27,7 +30,7 @@ roi = np.zeros_like(full_mask_img.get_data(),dtype=int)
 # get indices where mask and parcels overlap
 #indices = np.where((full_mask_img.get_data() > 0) & (parcels == 39) & (parcels == 35) & (parcels == 16) & (parcels == 36) & (parcels == 24) & (parcels == 37) & (parcels == 48) & (stats_mask.get_data() > 0))
 
-indices = np.where((full_mask_img.get_data() > 0) & (parcels == 43) & (stats_mask.get_data() > 0))
+indices = np.where((full_mask_img.get_data() > 0) & (parcels == idx) & (stats_mask.get_data() > 0))
  
 # fit match score and pvalue into brain
 roi[indices] = 1
