fe92f1f30a94bcafedadd642a0936bbb360908b8
jamalw <jamalw@princeton.edu>
2020-12-14 15:27:56 -0500

    added srm and torus code

--- music_feature_searchlight.py
+++ music_feature_searchlight.py
@@ -11,32 +11,37 @@ from sklearn import linear_model
 from scipy.spatial.distance import squareform, pdist
 import logging
 from scipy.fftpack import fft, ifft
+from srm import SRM_V1
 
 logger = logging.getLogger(__name__)
 
 datadir = '/jukebox/norman/jamalw/MES/'
 
+song_idx = int(sys.argv[1])
+runNum = int(sys.argv[2])
+
 subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
-# run 1 times
-song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511]) 
+if runNum == 0:
+    # run 1 times
+    song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511]) 
 
-songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
+    songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
-song_features = np.load(datadir + 'prototype/link/scripts/data/searchlight_input/chromaRun1_hrf.npy')
+    song_features = np.load(datadir + 'prototype/link/scripts/data/searchlight_input/torusRun1_hrf_34.npy')
 
-# run 2 times
-#song_bounds = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
+elif runNum == 1:
+    # run 2 times
+    song_bounds = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
 
-#songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
+    songs = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
 
-#song_features = np.load(datadir + 'prototype/link/scripts/data/searchlight_input/chromaRun2_hrf.npy')
+    song_features = np.load(datadir + 'prototype/link/scripts/data/searchlight_input/torusRun2_hrf_34.npy')
 
-song_idx = int(sys.argv[1])
 srm_k = 30
 hrf = 5
 
-mask_img = load_img(datadir + 'data/mask_nonan.nii.gz')
+mask_img = load_img(datadir + 'data/mask_nonan.nii')
 mask = mask_img.get_data()
 mask_reshape = np.reshape(mask,(91*109*91))
 
@@ -262,14 +267,16 @@ def RSA(X,song_features,song_idx,song_bounds,srm_k,hrf):
     """
     
     nPerm = 1000
+    n_iter = 10
     run1 = [X[i] for i in np.arange(0, int(len(X)/2))]
     run2 = [X[i] for i in np.arange(int(len(X)/2), len(X))]
-    print('Building Model')
-    srm = SRM(n_iter=10, features=srm_k)   
-    print('Training Model')
-    srm.fit(run2)
-    print('Testing Model')
-    shared_data = srm.transform(run1)
+    
+    # run SRM on sl data
+    if runNum == 0:
+        shared_data = SRM_V1(run2,run1,srm_k,n_iter)
+    elif runNum == 1:
+        shared_data = SRM_V1(run1,run2,srm_k,n_iter)   
+    
     shared_data = stats.zscore(np.dstack(shared_data),axis=1,ddof=1)
     
     data = np.mean(shared_data[:,song_bounds[song_idx]:song_bounds[song_idx + 1]],axis=2)
@@ -315,8 +322,15 @@ for j in range(voxmean.shape[1]):
  
 print('Saving data to Searchlight Folder')
 print(songs[song_idx])
-np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/chroma/raw/globals_raw_srm_k_' + str(srm_k) + '_fit_run2', results3d_real)
-np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/chroma/zscores/globals_z_srm_k' + str(srm_k) + '_fit_run2', results3d)
-np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/chroma/perms/globals_z_srm_k' + str(srm_k) + '_fit_run2', results3d_perms)
+
+if runNum == 0:
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/raw/globals_raw_srm_k_' + str(srm_k) + '_test_run1', results3d_real)
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/zscores/globals_z_srm_k' + str(srm_k) + '_test_run1', results3d)
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/perms/globals_z_srm_k' + str(srm_k) + '_test_run1', results3d_perms)
+if runNum == 1:
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/raw/globals_raw_srm_k_' + str(srm_k) + '_test_run2', results3d_real)
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/zscores/globals_z_srm_k' + str(srm_k) + '_test_run2', results3d)
+    np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/music_features/' + songs[song_idx] +'/torus/perms/globals_z_srm_k' + str(srm_k) + '_test_run2', results3d_perms)
+
 
 
