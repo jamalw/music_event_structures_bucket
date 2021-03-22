adf459b8b1c995205a2698c8f3e45ef0a6b52bc4
jamalw <jamalw@princeton.edu>
2021-03-22 15:13:14 -0400

    almost complete but needs a review before sending off

--- hmm_K_sweep_paper_parcels_no_boot.py
+++ hmm_K_sweep_paper_parcels_no_boot.py
@@ -12,13 +12,14 @@ import sys
 from scipy.ndimage.filters import gaussian_filter1d
 from statsmodels.nonparametric.kernel_regression import KernelReg
 
+roiNum = str(sys.argv[1])
 
 # Custom mean estimator with Fisher z transformation for correlations
 def fisher_mean(correlation, axis=None):
     return np.tanh(np.mean(np.arctanh(correlation), axis=axis))
 
 datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_input/parcels/Schaefer300/'
-ann_dirs = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/'
+savedir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/hmm_K_sweep_paper_results/Schaefer300/allROIs/'
 
 K_set = np.array((3,5,9,15,20,25,30,35,40,45))
 
@@ -39,9 +40,11 @@ songs_run2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_t
 
 durs_run2 = np.array([90,180,180,90,135,180,180,225,225,135,90,135,225,225,90,135])
 
+dict_names = ['WvA_Songs_x_Ks','Smooth_WvA','Max_WvA','Pref_Event_Length_Sec']
+
 # Load in data and reshape for Schaefer parcellations where the dimensionality is nSubjs X nVox X Time whereas the dimensions for the data used in the original version of the analysis was nVox X Time X nSubjs 
-run1 = np.load(datadir + 'parcel118_run1.npy')
-run2 = np.load(datadir + 'parcel118_run2.npy')
+run1 = np.load(datadir + 'parcel' + roiNum + '_run1.npy')
+run2 = np.load(datadir + 'parcel' + roiNum + '_run2.npy')
 
 nSubj = run1.shape[0]
 
@@ -55,8 +58,7 @@ for i in range(0,nSubj):
 run1_list = run1_list.copy()
 run2_list = run2_list.copy()
 
-wVa_results = np.zeros((16,len(K_set)))
-
+ROI_WvA = np.zeros((16,len(K_set)))
 
 n_iter = 50
 features = 10
@@ -112,64 +114,35 @@ for i in range(16):
             within = fisher_mean(cc[same_event*local_mask])
             across = fisher_mean(cc[(~same_event)*local_mask])
             within_across = within - across
-            wVa_results[i,j] = within_across
+            ROI_WvA[i,j] = within_across
 
-#np.save('/jukebox/norman/jamalw/MES/prototype/link/scripts/hmm_K_sweep_paper_results/Schaefer300/lprec118_wva' + str(bootNum), wVa_results)
-
-prec_data_list = []
-a1_data_list = []
-AG_data_list = []
-lvmpfc_data_list = []
-
-# load in each job (20 of which contain 50 bootstraps each which is 1000 boostraps total) for each ROI separately to be converted into one large matrix containing all bootstraps
-
-prec_data_jobNum = np.load(datadir + 'lprec_wva_regress_mfcc' + str(i) + '.npy')
-prec_data_list.append(prec_data_jobNum)
-a1_data_jobNum = np.load(datadir + 'rA1_wva_regress_mfcc' + str(i) + '.npy')
-a1_data_list.append(a1_data_jobNum)
-AG_data_jobNum = np.load(datadir + 'lAG_wva_regress_mfcc' + str(i) + '.npy')
-AG_data_list.append(AG_data_jobNum)
-#lvmpfc_data_jobNum = np.load(datadir + 'lvmpfc' + str(i) + '_no_fisher.npy')
-#lvmpfc_data_list.append(lvmpfc_data_jobNum)
-
-
-prec_data = np.dstack(prec_data_list)
-a1_data = np.dstack(a1_data_list)
-AG_data = np.dstack(AG_data_list)
-#lvmpfc_data = np.dstack(lvmpfc_data_list)
 
 sigma = '5'
 
-durs_run1 = np.array([225,90,180,135,90,180,135,90,180,135,225,90,225,225,180,135])
-
 durs_run1_new = durs_run1[:,np.newaxis]
 
-fairK = np.array((3,5,9,15,20,25,30,35,40,45))
-
-event_lengths = durs_run1_new/fairK
+event_lengths = durs_run1_new/K_set
 
 unique_event_lengths = np.unique(event_lengths)
 x = event_lengths.ravel()
 
-#ROI_data = [a1_data, AG_data, prec_data, lvmpfc_data]
-ROI_data = [a1_data,AG_data,prec_data]
-
 test_x = np.linspace(min(x), max(x), num=100)
-smooth_wva = np.zeros((len(unique_event_lengths), len(ROI_data)))
-
 
 opt_bw = 0
 
-for ROI in range(len(ROI_data)):
-    y = ROI_data[ROI][:,:].ravel()
-    KR = KernelReg(y,x,var_type='c')
-    opt_bw += KR.bw/len(ROI_data)
+y = ROI_WvA.ravel()
+KR = KernelReg(y,x,var_type='c')
+KR_w_bw = KernelReg(y,x,var_type='c', bw=KR.bw)
+max_wva = np.argmax(KR_w_bw.fit(test_x)[0])  # Find peak on fine grid
+smooth_wva = KR_w_bw.fit(unique_event_lengths)[0]
 
-max_wva = np.zeros(len(ROI_data))
-for ROI in range(len(ROI_data)):
-    y = ROI_data[ROI][:,:].ravel()
-    KR = KernelReg(y,x,var_type='c', bw=opt_bw)
-    max_wva[ROI] = np.argmax(KR.fit(test_x)[0])  # Find peak on fine grid
-    smooth_wva[:, ROI] += KR.fit(unique_event_lengths)[0]
+# compute rois preferred event length in seconds
+ROI_pref_sec = unique_event_lengths[np.argmax(smooth_wva)]
 
-np.save(datadir + 'smooth_wva_regress_mfcc',smooth_wva)
+inputs = [ROI_WvA,smooth_wva,max_wva,ROI_pref_sec]
+dct = {}
+
+for i,j in zip(dict_names,inputs):
+    dct.setdefault(i,[]).append(j)
+
+np.save(savedir + 'parcel' + roiNum + '_wva_data',dct)
