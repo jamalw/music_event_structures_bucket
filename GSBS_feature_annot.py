a6377dfab56872dd6eaba4519e6abd38176d5aa1
jamalw <jamalw@princeton.edu>
2021-04-14 12:51:06 -0400

    added combo feature fitting and moved save functions to end of script

--- GSBS_feature_annot.py
+++ GSBS_feature_annot.py
@@ -1,6 +1,7 @@
 from statesegmentation import GSBS
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import zscore
 
 song_bounds = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511])
 
@@ -26,18 +27,23 @@ for i in range(len(songs)):
     songChroma = chroma[:,song_bounds[i]:song_bounds[i+1]]
     songMFCC = mfcc[:,song_bounds[i]:song_bounds[i+1]]
     songTempo = tempo[:,song_bounds[i]:song_bounds[i+1]]
+    songCombo = zscore(np.vstack((songChroma,songMFCC,songTempo)),axis=1)    
 
     # compute feature boundaries
     print('computing chroma bounds for: ', songs[i])
     chromaBounds = GSBS_helper(songChroma, songChroma.shape[1])
-    np.save(savedir + songs[i] + '/chroma_bounds', chromaBounds) 
 
     print('computing mfcc bounds for: ', songs[i])
     mfccBounds   = GSBS_helper(songMFCC, songMFCC.shape[1])
-    np.save(savedir + songs[i] + '/mfcc_bounds', mfccBounds)
 
     print('computing tempo bounds for: ', songs[i])
     tempoBounds  = GSBS_helper(songTempo, songTempo.shape[1])   
-    np.save(savedir + songs[i] + '/tempo_bounds', tempoBounds)
     
-    
+    print('computing combined feature bounds for: '. songs[i])
+    comboBounds  = GSBS_helper(songCombo, songCombo.shape[1])   
+ 
+    # save bounds
+    np.save(savedir + songs[i] + '/chroma_bounds_kmax_all_timepoints', chromaBounds)
+    np.save(savedir + songs[i] + '/mfcc_bounds_kmax_all_timepoints', mfccBounds)
+    np.save(savedir + songs[i] + '/tempo_bounds_kmax_all_timepoints', tempoBounds)
+    np.save(savedir + songs[i] + '/combo_bounds_kmax_all_timepoints', comboBounds)        
