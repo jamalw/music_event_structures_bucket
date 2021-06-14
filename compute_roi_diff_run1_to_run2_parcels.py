153bb277b81e2645bf61754b337ec2ef293c0fc9
jamalw <jamalw@princeton.edu>
2021-06-14 16:28:08 -0400

    first completed version now performing t-test and FDR

--- compute_roi_diff_run1_to_run2_parcels.py
+++ compute_roi_diff_run1_to_run2_parcels.py
@@ -5,6 +5,57 @@ import glob
 import matplotlib.pyplot as plt
 import save_nifti
 
+parcelNum = 300
+
+datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/parcels/Schaefer' + str(parcelNum) + '/'
+
+parcels = nib.load("/jukebox/norman/jamalw/MES/data/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_" + str(parcelNum) + "Parcels_17Networks_order_FSLMNI152_2mm.nii.gz").get_data()
+
+mask = nib.load('/jukebox/norman/jamalw/MES/data/mask_nonan.nii')
+
+# Schaefer 200 bil mPFC
+#parcel_idx = [80,187]
+
+#Schaefer 300 DMN only
+parcel_idx = np.concatenate([np.arange(111,146),np.arange(271,294)])
+
+x = []
+y = []
+z = []
+
+x_single = []
+y_single = []
+z_single = []
+
+for i in range(len(parcel_idx)):
+    mask_x = np.where((mask.get_data() > 0) & (parcels == parcel_idx[i]))[0]
+    mask_y = np.where((mask.get_data() > 0) & (parcels == parcel_idx[i]))[1]
+    mask_z = np.where((mask.get_data() > 0) & (parcels == parcel_idx[i]))[2]
+    x.append(mask_x)
+    y.append(mask_y)
+    z.append(mask_z)
+    # take first set of coordinates from mask for each parcel 
+    x_single.append(mask_x[0])
+    y_single.append(mask_y[0])
+    z_single.append(mask_z[0])
+
+x_stack = np.hstack(x)
+y_stack = np.hstack(y)
+z_stack = np.hstack(z)
+
+indices = np.array((x_stack,y_stack,z_stack))
+
+x_single_stack = np.hstack(x_single)
+y_single_stack = np.hstack(y_single)
+z_single_stack = np.hstack(z_single)
+
+single_indices = np.array((x_single_stack,y_single_stack,z_single_stack))
+
+mask_reshaped = np.reshape(mask.get_data(),(91*109*91))
+tmap_final3D = np.zeros_like(mask.get_data(),dtype=float)
+pmap_final3D = np.zeros_like(mask.get_data(),dtype=float)
+qmap_final3D = np.zeros_like(mask.get_data(),dtype=float)
+
 def FDR_p(pvals):
     # Port of AFNI mri_fdrize.c
     assert np.all(pvals>=0) and np.all(pvals<=1)
@@ -41,61 +92,58 @@ def FDR_p(pvals):
     return qvals
 
 
-parcelNum = 200
-
-datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/'
-
-parcels = nib.load("/jukebox/norman/jamalw/MES/data/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_" + str(parcelNum) + "Parcels_17Networks_order_FSLMNI152_2mm.nii.gz").get_data()
-
-mask = nib.load('/jukebox/norman/jamalw/MES/data/mask_nonan.nii')
-
-# Schaefer 200 bil mPFC
-parcel_idx = [80,187]
-
-brain_mask = np.zeros_like(mask.get_data())
-
-for p in range(len(parcel_idx)):
-    brain_mask[(parcels == parcel_idx[p]) & (mask.get_data() > 0)] = 1
-
-# save mask (optional)
-save_nifti.save_nifti(brain_mask,mask.affine,'plots/17Network_200_Default_mPFC1.nii.gz')
-
 songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
-# initialize matrix which will store match scores for each song at every voxel
-voxels_by_songs_run1 = np.zeros((brain_mask[brain_mask == 1].shape[0],len(songs)))
-voxels_by_songs_run2 = np.zeros((brain_mask[brain_mask == 1].shape[0],len(songs))) 
+all_songs1D = np.zeros((len(single_indices[0]),len(songs)))
 
 for i in range(len(songs)):
     # load data for each run separately
-    data_run1 = np.load(datadir + songs[i] + '/zscores/globals_z_test_run1_split_merge_no_srm.npy')
-    data_run2 = np.load(datadir + songs[i] + '/zscores/globals_z_test_run2_split_merge_no_srm.npy')
+    data_run1 = nib.load(datadir + songs[i] + '/zscores_srm_v1_test_run1_pure_random_split_merge_no_srm.nii').get_data()
+    data_run2 = nib.load(datadir + songs[i] + '/zscores_srm_v1_test_run2_pure_random_split_merge_no_srm.nii').get_data()
+    all_songs1D[:,i] = data_run2[single_indices[0],single_indices[1],single_indices[2]] - data_run1[single_indices[0],single_indices[1],single_indices[2]]
     
-    # extract voxels from data for each run separately using brain mask
-    masked_data_run1 = data_run1[brain_mask == 1]
-    masked_data_run2 = data_run2[brain_mask == 1]
-
-    # store match scores across all voxels for song n
-    voxels_by_songs_run1[:,i] = masked_data_run1
-    voxels_by_songs_run2[:,i] = masked_data_run2
-
-# calculate song-specific match score differences between run 1 and run 2
-run1_vs_run2_diffs = voxels_by_songs_run2 - voxels_by_songs_run1
-
 # calculate tstats against zero for difference between match scores for 16 songs for run 2 minus run1 for each voxel separately and then do FDR
-tmap1D = np.zeros((run1_vs_run2_diffs.shape[0]))
-pmap1D = np.zeros((run1_vs_run2_diffs.shape[0]))
-qmap1D = np.zeros((run1_vs_run2_diffs.shape[0]))
+tmap1D = np.zeros((len(all_songs1D[:,0])))
+pmap1D = np.zeros((len(all_songs1D[:,0])))
+qmap1D = np.zeros((len(all_songs1D[:,0])))
 
-for j in range(run1_vs_run2_diffs.shape[0]):
-        tmap1D[j],pmap1D[j] = stats.ttest_1samp(run1_vs_run2_diffs[j,:],0,axis=0)
-        if run1_vs_run2_diffs[j,:].mean() > 0:
+for j in range(len(all_songs1D[:,0])):
+        tmap1D[j],pmap1D[j] = stats.ttest_1samp(all_songs1D[j,:],0,axis=0)
+        if all_songs1D[j,:].mean() > 0:
                 pmap1D[j] = pmap1D[j]/2
         else:
                 pmap1D[j] = 1-pmap1D[j]/2
 
+
 qmap1D = FDR_p(pmap1D)
- 
+
+# Fit data back into whole brain
+for i in range(len(parcel_idx)):
+    tmap_final3D[parcels==parcel_idx[i]] = tmap1D[i]
+    pmap_final3D[parcels==parcel_idx[i]] = pmap1D[i]
+    qmap_final3D[parcels==parcel_idx[i]] = qmap1D[i]
+
+# save data
+maxval = np.max(tmap_final3D)
+minval = np.min(tmap_final3D)
+img = nib.Nifti1Image(tmap_final3D, affine=mask.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + 'ttest_results/tstats_map_run2_minus_run1_pure_random_split_merge_original_match_score_no_srm_DMN.nii.gz')
+
+maxval = np.max(pmap_final3D)
+minval = np.min(pmap_final3D)
+img = nib.Nifti1Image(pmap_final3D, affine=mask.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + 'ttest_results/pstats_map_run2_minus_run1_pure_random_split_merge_original_match_score_no_srm_DMN.nii.gz')
+
+maxval = np.max(qmap_final3D)
+minval = np.min(qmap_final3D)
+img = nib.Nifti1Image(qmap_final3D, affine=mask.affine)
+img.header['cal_min'] = minval
+img.header['cal_max'] = maxval
+nib.save(img,datadir + 'ttest_results/qstats_map_run2_minus_run1_pure_random_split_merge_original_match_score_no_srm_DMN.nii.gz')
 
  
 
