b92ee0a1a58e510fec6f78f36a9ed79479db6f80
jamalw <jamalw@princeton.edu>
2021-06-15 12:36:38 -0400

    final version

--- compute_roi_diff_run1_to_run2.py
+++ compute_roi_diff_run1_to_run2.py
@@ -2,6 +2,8 @@ import numpy as np
 import nibabel as nib
 from scipy import stats
 import glob
+import matplotlib.pyplot as plt
+import save_nifti
 
 def FDR_p(pvals):
     # Port of AFNI mri_fdrize.c
@@ -39,7 +41,7 @@ def FDR_p(pvals):
     return qvals
 
 
-parcelNum = 100
+parcelNum = 200
 
 datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/'
 
@@ -47,13 +49,16 @@ parcels = nib.load("/jukebox/norman/jamalw/MES/data/CBIG/stable_projects/brain_p
 
 mask = nib.load('/jukebox/norman/jamalw/MES/data/mask_nonan.nii')
 
-# Schaefer 100 bil mPFC
-parcel_idx = [40,92]
+# Schaefer 200 bil mPFC
+parcel_idx = [80,187]
 
 brain_mask = np.zeros_like(mask.get_data())
 
 for p in range(len(parcel_idx)):
     brain_mask[(parcels == parcel_idx[p]) & (mask.get_data() > 0)] = 1
+
+# save mask (optional)
+save_nifti.save_nifti(brain_mask,mask.affine,'plots/17Network_200_Default_mPFC1.nii.gz')
 
 songs = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
@@ -91,8 +96,11 @@ for j in range(run1_vs_run2_diffs.shape[0]):
 
 qmap1D = FDR_p(pmap1D)
  
+# plot avg match scores for run 1 and run 2
+run_means = [voxels_by_songs_run1.mean(), voxels_by_songs_run2.mean()]
+run_stds = [voxels_by_songs_run1.std(), voxels_by_songs_run2.std()]
 
- 
+plt.bar(np.arange(2),run_means, yerr=run_stds) 
 
 
 
