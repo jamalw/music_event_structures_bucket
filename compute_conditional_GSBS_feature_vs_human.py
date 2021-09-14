1ae292c1bf8b432376ae0e69c7550a21757948a6
jamalw <jamalw@princeton.edu>
2021-09-14 13:48:31 -0400

    now printing mean of null

--- compute_conditional_GSBS_feature_vs_human.py
+++ compute_conditional_GSBS_feature_vs_human.py
@@ -53,47 +53,35 @@ for s in range(len(songs)):
     event_lengths = np.diff(np.concatenate(([0],human_bounds,[song_durs[s]]))) 
 
     # compute sum of matches between GSBS bounds and human bounds for each feature separately, counting as a match if 3 seconds away from each other
-    chroma_perm_bounds = chroma_bounds.copy()
-    mfcc_perm_bounds = mfcc_bounds.copy()
-    tempo_perm_bounds = tempo_bounds.copy() 
-    spect_perm_bounds = spect_bounds.copy()
-
-    chroma_event_lengths = np.diff(np.concatenate(([0],chroma_bounds,[song_durs[s]])))
-    mfcc_event_lengths = np.diff(np.concatenate(([0],mfcc_bounds,[song_durs[s]])))
-    tempo_event_lengths = np.diff(np.concatenate(([0],tempo_bounds,[song_durs[s]])))
-    spect_event_lengths = np.diff(np.concatenate(([0],spect_bounds,[song_durs[s]])))
 
     # add a 1 to human_bounds_matches variable if human bound matches a feature bound 
     for hb in human_bounds:
     # initalize/reset variable that will count number of feature to human matches to determine if a given human boundary has any match at all
         human_bounds_matches = 0
-        if np.any(np.abs(chroma_perm_bounds - hb) <= w):
+        if np.any(np.abs(chroma_bounds - hb) <= w):
             human_bounds_matches += 1
-        if np.any(np.abs(mfcc_perm_bounds - hb) <= w):
+        if np.any(np.abs(mfcc_bounds - hb) <= w):
             human_bounds_matches += 1
-        if np.any(np.abs(tempo_perm_bounds - hb) <= w):
+        if np.any(np.abs(tempo_bounds - hb) <= w):
             human_bounds_matches += 1
-        if np.any(np.abs(spect_perm_bounds - hb) <= w):
+        if np.any(np.abs(spect_bounds - hb) <= w):
             human_bounds_matches += 1
         if human_bounds_matches == 0:
             total_human_bounds_no_match += 1
 
     for p in range(nPerm+1):
         for hb in human_bounds:
-            if np.any(np.abs(chroma_perm_bounds - hb) <= w):
+            if np.any(np.abs(chroma_bounds - hb) <= w):
                 chroma_match[s,p] += 1
-            if np.any(np.abs(mfcc_perm_bounds - hb) <= w):
+            if np.any(np.abs(mfcc_bounds - hb) <= w):
                 mfcc_match[s,p] += 1
-            if np.any(np.abs(tempo_perm_bounds - hb) <= w):
+            if np.any(np.abs(tempo_bounds - hb) <= w):
                 tempo_match[s,p] += 1
-            if np.any(np.abs(spect_perm_bounds - hb) <= w):
+            if np.any(np.abs(spect_bounds - hb) <= w):
                 spect_match[s,p] += 1
 
         np.random.seed(p)
-        chroma_perm_bounds = np.cumsum(np.random.permutation(chroma_event_lengths))[:-1]
-        mfcc_perm_bounds = np.cumsum(np.random.permutation(mfcc_event_lengths))[:-1]
-        tempo_perm_bounds = np.cumsum(np.random.permutation(tempo_event_lengths))[:-1]
-        spect_perm_bounds = np.cumsum(np.random.permutation(spect_event_lengths))[:-1]
+        human_bounds = np.cumsum(np.random.permutation(event_lengths))[:-1]
 
 # sum matches across songs and permutations
 chroma_match_sum = np.sum(chroma_match,axis=0)
@@ -106,6 +94,7 @@ chroma_conditional = chroma_match_sum/total_chroma_bounds
 mfcc_conditional = mfcc_match_sum/total_mfcc_bounds
 tempo_conditional = tempo_match_sum/total_tempo_bounds
 spect_conditional = spect_match_sum/total_spect_bounds
+
 
 # compute conditional probability of human feature match given a human boundary
 chroma_human_conditional = chroma_match_sum/total_human_bounds    
@@ -124,7 +113,7 @@ axs[0,0].set_xlabel('p(human|chroma)',fontsize=14)
 axs[0,0].axvline(x=chroma_conditional[0], color='r', linewidth=3)
 chroma_conditional_pval = sc.norm.sf((chroma_conditional[0] - np.mean(chroma_conditional[1:]))/np.std(chroma_conditional[1:]))
 
-print('p(human|chroma) = ' + str(chroma_conditional[0]) + ' pval = ' + str(chroma_conditional_pval))
+print('p(human|chroma) = ' + str(chroma_conditional[0]) + ' pval = ' + str(chroma_conditional_pval) + ' null mean = ' + str(np.mean(chroma_conditional[1:])))
 
 im2 = axs[0,1].hist(mfcc_conditional[1:])
 axs[0,1].set_title('mfcc',fontweight='bold',fontsize=17)
@@ -132,7 +121,7 @@ axs[0,1].set_xlabel('p(human|mfcc)',fontsize=14)
 axs[0,1].axvline(x=mfcc_conditional[0], color='r', linewidth=3)
 mfcc_conditional_pval = sc.norm.sf((mfcc_conditional[0] - np.mean(mfcc_conditional[1:]))/np.std(mfcc_conditional[1:]))
 
-print('p(human|mfcc) = ' + str(mfcc_conditional[0]) + ' pval = ' + str(mfcc_conditional_pval))
+print('p(human|mfcc) = ' + str(mfcc_conditional[0]) + ' pval = ' + str(mfcc_conditional_pval) + ' null mean = ' + str(np.mean(mfcc_conditional[1:])))
 
 im3 = axs[1,0].hist(tempo_conditional[1:])
 axs[1,0].set_title('tempo',fontweight='bold',fontsize=17)
@@ -140,7 +129,7 @@ axs[1,0].set_xlabel('p(human|tempo)',fontsize=14)
 axs[1,0].axvline(x=tempo_conditional[0], color='r', linewidth=3)
 tempo_conditional_pval = sc.norm.sf((tempo_conditional[0] - np.mean(tempo_conditional[1:]))/np.std(tempo_conditional[1:]))
 
-print('p(human|tempo) = ' + str(tempo_conditional[0]) + ' pval = ' + str(tempo_conditional_pval))
+print('p(human|tempo) = ' + str(tempo_conditional[0]) + ' pval = ' + str(tempo_conditional_pval) + ' null mean = ' + str(np.mean(tempo_conditional[1:])))
 
 im4 = axs[1,1].hist(spect_conditional[1:])
 axs[1,1].set_title('spect',fontweight='bold',fontsize=17)
@@ -148,9 +137,9 @@ axs[1,1].set_xlabel('p(human|spectrogram)',fontsize=14)
 axs[1,1].axvline(x=spect_conditional[0], color='r', linewidth=3)
 spect_conditional_pval = sc.norm.sf((spect_conditional[0] - np.mean(spect_conditional[1:]))/np.std(spect_conditional[1:]))
 
-print('p(human|spect) = ' + str(spect_conditional[0]) + ' pval = ' + str(spect_conditional_pval))
+print('p(human|spect) = ' + str(spect_conditional[0]) + ' pval = ' + str(spect_conditional_pval) + ' null mean = ' + str(np.mean(spect_conditional[1:])))
 
-plt.savefig('plots/paper_versions/conditonal_feature_match_vs_total_feature_bounds_with_spect_fixed_null.png')
+#plt.savefig('plots/paper_versions/conditonal_feature_match_vs_total_feature_bounds_with_spect_fixed_null.png')
 
 # plot conditionals vs nulls
 fig, axs = plt.subplots(2,2,figsize=(17,17))
@@ -163,7 +152,7 @@ axs[0,0].set_xlabel('p(chroma match|human)',fontsize=14)
 axs[0,0].axvline(x=chroma_human_conditional[0], color='r', linewidth=3)
 chroma_human_conditional_pval = sc.norm.sf((chroma_human_conditional[0] - np.mean(chroma_human_conditional[1:]))/np.std(chroma_human_conditional[1:]))
 
-print('p(chroma match|human) = ' + str(chroma_human_conditional[0]) + ' pval = ' + str(chroma_human_conditional_pval))
+#print('p(chroma match|human) = ' + str(chroma_human_conditional[0]) + ' pval = ' + str(chroma_human_conditional_pval))
 
 im2 = axs[0,1].hist(mfcc_human_conditional[1:])
 axs[0,1].set_title('mfcc',fontweight='bold',fontsize=17)
@@ -171,7 +160,7 @@ axs[0,1].set_xlabel('p(mfcc match|human)',fontsize=14)
 axs[0,1].axvline(x=mfcc_human_conditional[0], color='r', linewidth=3)
 mfcc_human_conditional_pval = sc.norm.sf((mfcc_human_conditional[0] - np.mean(mfcc_human_conditional[1:]))/np.std(mfcc_human_conditional[1:]))
 
-print('p(mfcc match|human) = ' + str(mfcc_human_conditional[0]) + ' pval = ' + str(mfcc_human_conditional_pval))
+#print('p(mfcc match|human) = ' + str(mfcc_human_conditional[0]) + ' pval = ' + str(mfcc_human_conditional_pval))
 
 im3 = axs[1,0].hist(tempo_human_conditional[1:])
 axs[1,0].set_title('tempo',fontweight='bold',fontsize=17)
@@ -179,7 +168,7 @@ axs[1,0].set_xlabel('p(tempo match|human)',fontsize=14)
 axs[1,0].axvline(x=tempo_human_conditional[0], color='r', linewidth=3)
 tempo_human_conditional_pval = sc.norm.sf((tempo_human_conditional[0] - np.mean(tempo_human_conditional[1:]))/np.std(tempo_human_conditional[1:]))
 
-print('p(tempo match|human) = ' + str(tempo_human_conditional[0]) + ' pval = ' + str(tempo_human_conditional_pval))
+#print('p(tempo match|human) = ' + str(tempo_human_conditional[0]) + ' pval = ' + str(tempo_human_conditional_pval))
 
 im4 = axs[1,1].hist(spect_human_conditional[1:])
 axs[1,1].set_title('spectrogram',fontweight='bold')
@@ -187,9 +176,9 @@ axs[1,1].set_xlabel('p(spectrogram match|human)',fontsize=14)
 axs[1,1].axvline(x=spect_human_conditional[0], color='r', linewidth=3)
 spect_human_conditional_pval = sc.norm.sf((spect_human_conditional[0] - np.mean(spect_human_conditional[1:]))/np.std(spect_human_conditional[1:]))
 
-print('p(spect match|human) = ' + str(spect_human_conditional[0]) + ' pval = ' + str(spect_human_conditional_pval))
+#print('p(spect match|human) = ' + str(spect_human_conditional[0]) + ' pval = ' + str(spect_human_conditional_pval))
 
-plt.savefig('plots/paper_versions/conditonal_feature_match_vs_total_human_bounds_with_spect_fixed_null.png')
+#plt.savefig('plots/paper_versions/conditonal_feature_match_vs_total_human_bounds_with_spect_fixed_null.png')
 
 
 
