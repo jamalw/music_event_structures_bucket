a2e7a56b9639d52c98608dd1563c893238bb2c5e
jamalw <jamalw@princeton.edu>
2022-01-12 17:55:47 -0500

    removing srm from script

--- plot_voxel_hist_run1_to_run2.py
+++ plot_voxel_hist_run1_to_run2.py
@@ -15,84 +15,81 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 datadir = '/jukebox/norman/jamalw/MES/prototype/link/scripts/chris_dartmouth/data/'
 ann_dirs = '/jukebox/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_K_sweep_srm/'
+roi = bil_A1
 
-song_number = int(sys.argv[1])
+for i in range(16):
+    song_number = i
 
-do_srm = int(sys.argv[2])
+    #do_srm = int(sys.argv[2])
 
-runNum = int(sys.argv[3])
+    hrf = 0
 
-print(song_number)
+    # run 1 durations
+    durs1 = np.array([225,89,180,134,90,180,134,90,179,135,224,89,224,225,179,134])
 
-hrf = 0
+    songs1 = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
 
-# run 1 durations
-durs1 = np.array([225,89,180,134,90,180,134,90,179,135,224,89,224,225,179,134])
+    print(songs1[song_number])
 
-songs1 = ['Finlandia', 'Blue_Monk', 'I_Love_Music','Waltz_of_Flowers','Capriccio_Espagnole','Island','All_Blues','St_Pauls_Suite','Moonlight_Sonata','Symphony_Fantastique','Allegro_Moderato','Change_of_the_Guard','Boogie_Stop_Shuffle','My_Favorite_Things','The_Bird','Early_Summer']
+    # run 1 times
+    song_bounds1 = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511])
 
-# run 1 times
-song_bounds1 = np.array([0,225,314,494,628,718,898,1032,1122,1301,1436,1660,1749,1973, 2198,2377,2511])
+    songs2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
 
-songs2 = ['St_Pauls_Suite', 'I_Love_Music', 'Moonlight_Sonata', 'Change_of_the_Guard','Waltz_of_Flowers','The_Bird', 'Island', 'Allegro_Moderato', 'Finlandia', 'Early_Summer', 'Capriccio_Espagnole', 'Symphony_Fantastique', 'Boogie_Stop_Shuffle', 'My_Favorite_Things', 'Blue_Monk','All_Blues']
+    # run 2 times
+    song_bounds2 = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
 
-# run 2 times
-song_bounds2 = np.array([0,90,270,449,538,672,851,1031,1255,1480,1614,1704,1839,2063,2288,2377,2511])
+    human_bounds = np.load(ann_dirs + songs1[song_number] + '/' + songs1[song_number] + '_beh_seg.npy') + hrf
 
-human_bounds = np.load(ann_dirs + songs1[song_number] + '/' + songs1[song_number] + '_beh_seg.npy') + hrf
+    human_bounds = np.append(0,np.append(human_bounds,durs1[song_number])) 
 
-human_bounds = np.append(0,np.append(human_bounds,durs1[song_number])) 
+    start_run1 = song_bounds1[song_number]
+    end_run1   = song_bounds1[song_number+1]
 
-start_run1 = song_bounds1[song_number]
-end_run1   = song_bounds1[song_number+1]
+    start_run2 = song_bounds2[songs2.index(songs1[song_number])]
+    end_run2 = song_bounds2[songs2.index(songs1[song_number]) + 1]
 
-start_run2 = song_bounds2[songs2.index(songs1[song_number])]
-end_run2 = song_bounds2[songs2.index(songs1[song_number]) + 1]
+    # Load in data
+    run1 = stats.zscore(np.load(datadir + 'fdr_01_' + roi + '_split_merge_no_srm_run1_n25.npy'),axis=1, ddof=1)
+    run2 = stats.zscore(np.load(datadir + 'fdr_01_' + roi + '_split_merge_no_srm_run2_n25.npy'), axis=1, ddof=1)
 
-roi = 'bil_aud'
+    if do_srm == 0:
+        run1DataAvg = np.mean(run1,axis=2)
+        run2DataAvg = np.mean(run2,axis=2)
 
-# Load in data
-run1 = stats.zscore(np.load(datadir + 'fdr_01_' + roi + '_split_merge_no_srm_run1_n25.npy'),axis=1, ddof=1)
-run2 = stats.zscore(np.load(datadir + 'fdr_01_' + roi + '_split_merge_no_srm_run2_n25.npy'), axis=1, ddof=1)
+        song1 = run1DataAvg[:,start_run1:end_run1]
+        song2 = run2DataAvg[:,start_run2:end_run2]
+    #elif do_srm == 1:
+        # Convert data into lists where each element is voxels by samples
+        #run1_list = []
+        #run2_list = []
+        #for i in range(0,run1.shape[2]):
+            #run1_list.append(run1[:,:,i])
+            #run2_list.append(run2[:,:,i])
 
-if do_srm == 0:
-    run1DataAvg = np.mean(run1,axis=2)
-    run2DataAvg = np.mean(run2,axis=2)
+        #n_iter = 10
+        #features = 10
 
-    song1 = run1DataAvg[:,start_run1:end_run1]
-    song2 = run2DataAvg[:,start_run2:end_run2]
-elif do_srm == 1:
-    # Convert data into lists where each element is voxels by samples
-    run1_list = []
-    run2_list = []
-    for i in range(0,run1.shape[2]):
-        run1_list.append(run1[:,:,i])
-        run2_list.append(run2[:,:,i])
+        ## run SRM on ROIs
+        #shared_data_test1 = SRM_V1(run2_list,run1_list,features,n_iter)
+        #shared_data_test2 = SRM_V1(run1_list,run2_list,features,n_iter)
 
-    n_iter = 10
-    features = 10
+        #avg_response_test_run1 = sum(shared_data_test1)/len(shared_data_test1)
+        #avg_response_test_run2 = sum(shared_data_test2)/len(shared_data_test2)
 
-    ## run SRM on ROIs
-    shared_data_test1 = SRM_V1(run2_list,run1_list,features,n_iter)
-    shared_data_test2 = SRM_V1(run1_list,run2_list,features,n_iter)
+        song1 = avg_response_test_run1[:,start_run1:end_run1]
+        song2 = avg_response_test_run2[:,start_run2:end_run2]
 
-    avg_response_test_run1 = sum(shared_data_test1)/len(shared_data_test1)
-    avg_response_test_run2 = sum(shared_data_test2)/len(shared_data_test2)
+        # perform correlation on all voxels between run1 and run2
+        corrs_run1_run2 = np.zeros(song1.shape[0])
 
-    song1 = avg_response_test_run1[:,start_run1:end_run1]
-    song2 = avg_response_test_run2[:,start_run2:end_run2]
+        for i in range(song1.shape[0]):
+            corrs_run1_run2[i] = np.corrcoef(song1[i,:],song2[i,:])[0][1]
 
-if runNum == 0:
-    data = song1
-elif runNum == 1:
-    data = song2
 
 plt.figure(figsize=(12,12))
 ax = plt.gca()
-im = ax.imshow(np.corrcoef(data.T),aspect='equal')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.05)
-cbar = plt.colorbar(im, cax=cax)
+im = ax.hist(corrs_run1_run2)
 
 for t in cbar.ax.get_yticklabels():
      t.set_fontsize(31)
@@ -109,15 +106,15 @@ ax.set_ylabel('TRs',fontsize=33)
 plt.setp(ax.get_xticklabels(), fontsize=32,rotation=45)
 plt.setp(ax.get_yticklabels(), fontsize=32)
 
-if do_srm == 0:
-    ax.set_title('bil mPFC', fontsize=45,y=1.015,fontweight='bold')
-    #ax.set_title('bil prec ' + song_names[song_number] + ' run ' + str(runNum + 1),fontsize=36,y=1.015)
-    plt.tight_layout()
-    plt.savefig('plots/paper_versions/debug/no_srm_sl/' + roi + '/separate_runs/no_SRM/' + song_names[song_number] + '_' + roi + '_no_SRM_run' + str(runNum + 1))
-elif do_srm == 1:   
-    ax.set_title(roi + ' ' + song_names[song_number] + ' Run ' + str(runNum + 1),fontsize=32,fontweight='bold')    
-    plt.savefig('plots/paper_versions/debug/no_srm_sl/' + roi + '/separate_runs/with_SRM/' + song_names[song_number] + '_' + roi + '_with_SRM_run' + str(runNum + 1))
-
-
+#if do_srm == 0:
+#    ax.set_title('bil mPFC', fontsize=45,y=1.015,fontweight='bold')
+#    ax.set_title('bil prec ' + song_names[song_number] + ' run ' + str(runNum + 1),fontsize=36,y=1.015)
+#    plt.tight_layout()
+#    plt.savefig('plots/paper_versions/debug/no_srm_sl/' + roi + '/separate_runs/no_SRM/' + song_names[song_number] + '_' + roi + '_no_SRM_run' + str(runNum + 1))
+#elif do_srm == 1:   
+#    ax.set_title(roi + ' ' + song_names[song_number] + ' Run ' + str(runNum + 1),fontsize=32,fontweight='bold')    
+#    plt.savefig('plots/paper_versions/debug/no_srm_sl/' + roi + '/separate_runs/with_SRM/' + song_names[song_number] + '_' + roi + '_with_SRM_run' + str(runNum + 1))
+#
+#
 
 
